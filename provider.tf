6ea505b459b9faef66e86a096f22e916eff582d6
Basil Pozdeev <basil.pozdeev@ctac.nl>
2022-11-30 23:03:54 +0100

    AWS Provider

--- provider.tf
+++ provider.tf
@@ -10,9 +10,6 @@ terraform {
   }
 }
 
-# An example resource that does nothing.
-resource "null_resource" "example" {
-  triggers = {
-    value = "A example resource that does nothing!"
-  }
+provider "aws" {
+
 }
