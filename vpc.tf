b1aa8063cc7ad96a82d74660093a176a53341881
Basil Pozdeev <basil.pozdeev@ctac.nl>
2022-11-30 23:07:41 +0100

    Empty VPC

---
+++ vpc.tf
@@ -0,0 +1,11 @@
+module "main_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "vpc-anc-dev"
+  cidr = "10.0.0.0/16"
+
+  azs                 = ["eu-central-1a"]
+  public_subnets      = ["10.0.1.0/24"]
+  public_subnet_names = ["subnet-main-private"]
+
+}
