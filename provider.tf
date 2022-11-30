51289194f5d9171150b9f7bfa0a269a4b404fcf1
Basil Pozdeev <basil.pozdeev@ctac.nl>
2022-11-30 23:27:37 +0100

    inline iam creds

--- provider.tf
+++ provider.tf
@@ -11,5 +11,7 @@ terraform {
 }
 
 provider "aws" {
-  region = "eu-central-1"
+  region     = "eu-central-1"
+  access_key = var.access_key
+  secret_key = var.secret_key
 }
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "access_key" {
+  type        = string
+  description = "IAM access key"
+}
+
+variable "secret_key" {
+  type        = string
+  description = "IAM access key"
+}
