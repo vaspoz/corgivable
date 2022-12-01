a5a37b52f7e3694a627437fc8cb3383200f4a298
Basil Pozdeev <basil.pozdeev@ctac.nl>
2022-12-01 15:56:16 +0100

    Cleaned up the env

--- provider.tf
+++ provider.tf
@@ -3,19 +3,10 @@ terraform {
     organization = "basil-home"
 
     workspaces {
-      name = "corgivable"
+      name = "corgifun"
     }
   }
 
-  # backend "remote" {
-  #   # The name of your Terraform Cloud organization.
-  #   organization = "basil-home"
-
-  #   # The name of the Terraform Cloud workspace to store Terraform state files in.
-  #   workspaces {
-  #     name = "corgivable"
-  #   }
-  # }
 }
 
 provider "aws" {
