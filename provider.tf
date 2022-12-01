d00612a8038fa9a02fc6b6b9323c609c9e4f1bbc
Basil Pozdeev <basil.pozdeev@ctac.nl>
2022-12-01 15:45:24 +0100

    test

--- provider.tf
+++ provider.tf
@@ -1,13 +1,20 @@
 terraform {
-  backend "remote" {
-    # The name of your Terraform Cloud organization.
+  cloud {
     organization = "basil-home"
-
-    # The name of the Terraform Cloud workspace to store Terraform state files in.
-    workspaces {
-      name = "corgivable"
-    }
   }
+
+  workspaces {
+    name = "corgivable"
+  }
+  # backend "remote" {
+  #   # The name of your Terraform Cloud organization.
+  #   organization = "basil-home"
+
+  #   # The name of the Terraform Cloud workspace to store Terraform state files in.
+  #   workspaces {
+  #     name = "corgivable"
+  #   }
+  # }
 }
 
 provider "aws" {
