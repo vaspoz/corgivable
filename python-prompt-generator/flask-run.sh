5aea0f6402644464abacc8b1668b2d2d37907000
Basil Pozdeev <basil.pozdeev@ctac.nl>
2022-12-03 21:19:40 +0100

    added python Flusk

---
+++ python-prompt-generator/flask-run.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+if [[ "${TRACE-0}" == "1" ]]; then
+	    set -o xtrace
+fi
+
+if [[ "${1-}" =~ ^-*h(elp)?$ ]]; then
+	    echo 'Usage: ./script.sh arg-one arg-two
+
+	    This is an awesome bash script to make your life better.
+
+	    '
+	        exit
+fi
+
+cd "$(dirname "$0")"
+
+main() {
+	export FLASK_APP=prompt-generator.py
+	export FLASK_ENV=production
+	echo done
+}
+
+main "$@"
+
